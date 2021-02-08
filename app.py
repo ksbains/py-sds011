aef00f0e2fe12b4d4f31d5c568f2355042c9974f
Karanbir Bains <karanbir.bains@sjsu.edu>
2021-02-07 16:10:40 -0800

    was able to create a CSV file with the PMT data. will be able to set proper time for recording the data and then analyze the time seris data from csv format

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from sds011 import *
 sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
+import datetime
+import csv
 
-pmt_2_5, pmt_10 = sensor.query()
+with open('pmtData','w', newline='') as f:
+    thewriter = csv.writer(f)
 
-print("The pmt values are:")
-print("PMT 2.5: " + str(pmt_2_5))
-print("PMT 10: " + str(pmt_10))
+    thewriter.writerow(["time", "pmt 2.5", "pmt 10"])
+
+    for i in range(1, 10):
+        ct = datetime.datetime.now()
+        pmt_2_5, pmt_10 = sensor.query()
+        thewriter.writerow([str(ct), str(pmt_2_5), str(pmt_10)])
+
+
+
+
+# print("The pmt values are:")
+# print("PMT 2.5: " + str(pmt_2_5))
+# print("PMT 10: " + str(pmt_10))
 
