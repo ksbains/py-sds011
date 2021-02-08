d0a3cdd01cdf9d1a4926f2afa0f54858be43f095
Karanbir Bains <karanbir.bains@sjsu.edu>
2021-02-07 16:31:24 -0800

    able to schedule the app to record every 30 seconds, now need to automate the creation and storage of daily files

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from sds011 import *
 sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
 import datetime
+import time
+import schedule
 import csv
+
+def record():
+    ct = datetime.datetime.now()
+    pmt_2_5, pmt_10 = sensor.query()
+    print(str(ct), str(pmt_2_5), str(pmt_10))
+    with open('pmtData','w', newline='') as f:
+        thewriter = csv.writer(f)
+        thewriter.writerow([str(ct), str(pmt_2_5), str(pmt_10)])
+
 
 with open('pmtData','w', newline='') as f:
     thewriter = csv.writer(f)
-
     thewriter.writerow(["time", "pmt 2.5", "pmt 10"])
+    
 
-    for i in range(1, 10):
-        ct = datetime.datetime.now()
-        pmt_2_5, pmt_10 = sensor.query()
-        thewriter.writerow([str(ct), str(pmt_2_5), str(pmt_10)])
+schedule.every(30).seconds.do(record)
 
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
 
 
 
