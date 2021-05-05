bfced48c1e2ce2803bd5b423e4331f120a15cf5e
Karanbir Bains <karanbir.bains@sjsu.edu>
2021-05-04 21:51:11 -0700

    changed the values of the temp and humidity props until we have the sensor to gather the data. Also changed the recording of data to be every 30 seconds again

--- sensor.py
+++ sensor.py
@@ -11,13 +11,13 @@ username = "killah-bee"
 def recordData():
     ct = datetime.now()
     pmt_2_5, pmt_10 = sensor.query()
-    temp = "80"
-    humidity = "40"
+    temp = "invalid"
+    humidity = "invalid"
     print(username,str(pmt_2_5),str(pmt_10), temp, humidity, str(ct))
     addSensor(username, str(pmt_2_5),str(pmt_10), temp, humidity, str(ct))
 
 # every 10 seconds record data to dict
-schedule.every(5).seconds.do(recordData)
+schedule.every(30).seconds.do(recordData)
 # every minute create and fill file with data. 
 # schedule.every(24).hours.do(recordData)
 
